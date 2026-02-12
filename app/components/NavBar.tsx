1e34057a1a70c845866655171410ffe64a354962
Jinxy2004 <jinxy2006@yahoo.com>
2026-02-12 18:21:06 -0500

    Fixed navbar hiding other pages.

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -3,7 +3,7 @@ import { ThemeToggle } from "./ThemeToggle";
 
 export default function NavBar() {
   return (
-    <div className="fixed top-0 w-full border-b border-foreground/20 bg-background py-5 px-24">
+    <div className="sticky top-0 z-50 w-full border-b border-foreground/20 bg-background py-5 px-24">
       <div className="relative flex items-center justify-center">
         <ul className="flex gap-10 text-lg">
           <Link href="/about">About Page</Link>
