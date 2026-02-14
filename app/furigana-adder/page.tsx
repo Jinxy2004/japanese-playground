7b4666ae1dac1a74d575afc0705ee6729cb0b324
Jinxy2004 <jinxy2006@yahoo.com>
2026-02-13 23:59:19 -0500

    Added text input box

--- app/furigana-adder/page.tsx
+++ app/furigana-adder/page.tsx
@@ -1,7 +1,10 @@
 export default function FuriganaAdder() {
   return (
-    <div className="flex h-screen items-center justify-center">
-      <h1 className="text-4xl font-bold">Furigana Adder</h1>
+    <div className="flex h-screen items-start justify-center py-5">
+      <textarea
+        className="w-[30%] h-[30%] bg-foreground/30 border border-gray-100 rounded-md px-3
+      align-text-top"
+      ></textarea>
     </div>
   );
 }
