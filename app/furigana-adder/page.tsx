7001b36147f2fadbb4cc1b9dad973a5fd60539a1
Jinxy2004 <jinxy2006@yahoo.com>
2026-02-14 23:45:31 -0500

    Added text area text-capturing.

--- app/furigana-adder/page.tsx
+++ app/furigana-adder/page.tsx
@@ -1,10 +1,25 @@
+"use client";
+
+import { useRef } from "react";
+
 export default function FuriganaAdder() {
+  // Stores a const reference to the text area object
+  const textareaRef = useRef<HTMLTextAreaElement>(null);
+
+  function getTextFromTextArea() {
+    const text = textareaRef.current?.value;
+    console.log(text);
+  }
+
   return (
     <div className="flex h-screen items-start justify-center py-5">
       <textarea
+        ref={textareaRef} // Tells the textareaRef to refer to this textarea
+        id="textbox"
         className="w-[30%] h-[30%] bg-foreground/30 border border-gray-100 rounded-md px-3
       align-text-top"
       ></textarea>
+      <button onClick={getTextFromTextArea}>Get Text</button>
     </div>
   );
 }
