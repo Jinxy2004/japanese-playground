0df8ca69ed2b22a7da39bc5329ba45e59aae3553
Jinxy2004 <jinxy2006@yahoo.com>
2026-02-18 20:06:39 -0500

    Fixed nav bar.

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -4,17 +4,23 @@ import Button from "./Button";
 
 export default function NavBar() {
   return (
-    <div className="sticky top-0 z-50 w-full border-b border-foreground/20 bg-background py-5 px-24">
-      <div className="relative flex items-center justify-center">
-        <ul className="flex gap-10 text-lg">
+    /*
+This navbar works by first creating an outer div which houses everything. Then we create a div that will house
+our other buttons. The css works by first creating a grid of 3 columns and having each item in it be centered
+(in terms of the x axis). Then for the ul we justify-center so that the buttons are in the center, then for the
+other buttons we justify-end so that they stick to the end
+    */
+    <div className="sticky top-0 w-full border-b border-foreground/20 bg-background px-8 py-5">
+      <div className="grid grid-cols-3 items-center">
+        <div />
+
+        <ul className="flex justify-center gap-1.5 text-lg">
           <NavBarLink page="about" pageName="About" />
           <NavBarLink page="furigana-adder" pageName="Furigana Adder" />
         </ul>
 
-        <div className="absolute right-35">
+        <div className="flex justify-end gap-1.5">
           <ThemeToggle />
-        </div>
-        <div className="absolute right-0">
           <Button btnText="Sign Up/Sign In" path="/user-account/signup" />
         </div>
       </div>
